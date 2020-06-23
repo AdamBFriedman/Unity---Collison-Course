d36d9044f996aa9ab282232389357992f0a86ad6
Adam Friedman <adam@blok.party>
2020-06-23 15:19:30 -0700

    Updated score to show player's z position

---
+++ Rolling Ball Game/Assets/Scripts/Score.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+// Bring in for UI
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    // Reference movement of our player
+    public Transform player;
+
+    public Text scoreText;
+    // Update is called once per frame
+    void Update()
+    {
+        // Update text with the players movement
+        scoreText.text = player.position.z.ToString("0");
+    }
+}
