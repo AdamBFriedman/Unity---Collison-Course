835de04b3a4a190c20ac971134bfa22cbabe716f
Adam Friedman <adam@blok.party>
2020-06-23 10:56:08 -0700

    Forgot to create repo yesterday - whoops!

---
+++ Rolling Ball Game/Assets/FollowPlayer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform player;
+    // Vector3 (stores 3 floats)
+    public Vector3 offset;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // lowercase transform refers to this GameObject
+        // This will position the camera to the player and then add the offset so that it's behind and higher (following the player)
+        transform.position = player.position + offset;
+    }
+}
