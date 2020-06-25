66302585edf4cb12314d8879283f8b5c378d8862
Adam Friedman <adam@blok.party>
2020-06-25 13:27:29 -0700

    Added pause script

---
+++ Rolling Ball Game/Assets/Scripts/Pause.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Pause game on space
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Time.timeScale = 0;
+        }
+
+        // Resume game on c
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            Time.timeScale = 1;
+        }
+
+    }
+}
